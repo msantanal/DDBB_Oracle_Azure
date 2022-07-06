caacfe704b6885e4e483435d67d60d8f5ee02672
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-06 17:04:57 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "oraclepaas_database_service_instance" "bdoracle" {
+  name              = var.dboracle_name
+  edition           = var.edition
+  shape             = var.shape
+  subscription_type = var.subscription_type
+  version           = var.version
+  vm_public_key     = var.vm_public_key
+  
+  database_configuration {
+      username           = var.username
+      admin_password     = var.admin_password
+      sid                = var.sid
+      backup_destination = "NONE"
+      usable_storage     = var.usable_storage
+      depend on          = [module.KEYVAULT]
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,30 @@
+variable "dboracle_name" {
+  type        = string
+}
+variable "edition" {
+  type        = string
+}
+variable "shape" {
+  type        = string
+}
+variable "subscription_type" {
+  type        = string
+}
+variable "version" {
+  type        = string
+}
+variable "vm_public_key" {
+  type        = string
+}
+variable "username" {
+  type        = string
+}
+variable "admin_password" {
+  type        = string
+}
+variable "sid" {
+  type        = string
+}
+variable "usable_storage" {
+  type        = string
+}
